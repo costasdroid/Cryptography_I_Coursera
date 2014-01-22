2a091f4eb9d0f5868dea728b6c72b6de180fe5a4
costasdroid <costasdroid@gmail.com>
2014-01-22 22:19:52 +0200

    optimization of code

--- Week3/Problem1.java
+++ Week3/Problem1.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.security.MessageDigest;
 
@@ -5,45 +6,29 @@ public class Problem1 {
 	public static void main(String[] args) throws Exception {
 		
 		//The file we are going to hash
-		String fileName = "C:\\filepath\\6 - 1 - Introduction (11 min).mp4";
-		
-		//We read the file 2 times
-		//1st for byte count
-		//2nd for hashing blocks
-		FileInputStream fis = new FileInputStream(fileName);
+		String fileName = "C:\\Users\\Costis\\Downloads\\6 - 1 - Introduction (11 min).mp4";
 		
 		//The variable totalBytes stores the Bytes of the file
-		long totalBytes = 0;
+		File file = new File(fileName);
+		long totalBytes = file.length();
 		
-		//nRead is the number of blocks the read command returns
-		int nread = 0;
+		//opening the file
+		FileInputStream fis = new FileInputStream(fileName);
 		
-		//we set the buffer to 1024 bytes = 1 KB
-		byte[] temp = new byte[1024];
+		//Block size
+		int bSize = 1024;
 		
-		//1st pass
-		while ((nread = fis.read(temp)) != -1) {
-			totalBytes = totalBytes + nread;
-		}
+		//the last block is totalBytes mod bSize long
+		int lastBlock = (int) (totalBytes % bSize);
 		
-		//and close the file
-		fis.close();
-		
-		//reopening the file
-		fis = new FileInputStream(fileName);
-		
-		//the last block is totalBytes mod 1024 bytes long
-		int lastBlock = (int) (totalBytes % 1024);
-		
-		//We create the 2 dim matrix of totalBytes/1024 - 1KB buffers		
-		byte[][] dataBytes = new byte[(int) (totalBytes / 1024)][1024];
-		
-		//and the extra last one <=1KB
+		//We create the 2 dim matrix of (totalBytes/bSize x bSizeKB buffers)
+		//and the extra last one < bSizeKB
+		byte[][] dataBytes = new byte[(int) (totalBytes / bSize)][bSize];
 		byte[] dB = new byte[lastBlock];
 		
-		//2nd pass buffering blocks
-		for (int k = 0; k < (int) (totalBytes / 1024); k++) {
-			nread = fis.read(dataBytes[k]);
+		//pass buffering blocks
+		for (int k = 0; k < (int) (totalBytes / bSize); k++) {
+			fis.read(dataBytes[k]);
 		}
 		//and the last block
 		fis.read(dB);
@@ -56,11 +41,10 @@ public class Problem1 {
 		
 		//we digest the lastBlock and create the first hash
 		md.update(dB, 0, lastBlock);
-		sha = md.digest();
-		
+		sha = md.digest();	
 		//and until the beginning, we consume each block concatenated with the previous digest
-		for (int k = 0; k < (int) (totalBytes / 1024); k++) {
-			md.update(dataBytes[(int) (totalBytes / 1024) - k - 1], 0, 1024);
+		for (int k = 0; k < (int) (totalBytes / bSize); k++) {
+			md.update(dataBytes[(int) (totalBytes / bSize) - k - 1], 0, bSize);
 			md.update(sha, 0 ,32);
 			sha = md.digest();
 		}
@@ -71,8 +55,7 @@ public class Problem1 {
 		//We convert each byte of the last hash key to hex string
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < sha.length; i++) {
-			sb.append(Integer.toString((sha[i] & 0xff) + 0x100, 16)
-					.substring(1));
+			sb.append(((sha[i] & 0xff) < 16 ? "0" : "") + Integer.toHexString(sha[i] & 0xff));
 		}
 		
 		//The output
